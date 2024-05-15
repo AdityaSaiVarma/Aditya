237e49f35a2008b7ab65bbf60f3cc53407c834fc
AdityaSaiVarma <varma25072000@gmail.com>
2024-05-15 13:21:27 -0500

    Commiting Java Prgm

---
+++ Demo.java
@@ -0,0 +1,21 @@
+public class Sample1 {
+    int a = 10;
+    static float b = 20.0f;
+    Double d = 30.0;
+    void add(){
+        System.out.println("The value of a is :"+a);
+        System.out.println("The value of a is :"+b);
+        System.out.println("The value of d is :"+d);
+
+        public static void main(String args[]){
+
+            Sample1 s1 = new Sample1();
+            s1.add();
+
+        }
+
+
+    }
+
+
+}
